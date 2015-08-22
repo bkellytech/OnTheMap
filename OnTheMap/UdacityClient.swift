4c6e6952285714ab689e46683e3c5ec43210cdad
Kelly Egan <kelly.egan@gmail.com>
2015-08-22 13:55:46 -0400

    Create UdacityClient class for managing data from Udacity.

---
+++ OnTheMap/UdacityClient.swift
@@ -0,0 +1,20 @@
+//
+//  UdacityClient.swift
+//  OnTheMap
+//
+//  Created by Kelly Egan on 8/22/15.
+//  Copyright (c) 2015 Kelly Egan. All rights reserved.
+//
+
+import Foundation
+
+class UdacityClient: NSObject {
+    
+    //Singleton
+    static let sharedInstance = UdacityClient()
+    
+    override init() {
+        super.init()
+    }
+    
+}
