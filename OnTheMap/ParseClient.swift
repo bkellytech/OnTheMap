3af31ad63aa8412f72570c8559d955e6d95f362e
Kelly Egan <kelly.egan@gmail.com>
2015-08-23 12:22:10 -0400

    Add ParseClient skeleton

---
+++ OnTheMap/ParseClient.swift
@@ -0,0 +1,18 @@
+//
+//  ParseClient.swift
+//  OnTheMap
+//
+//  Created by Kelly Egan on 8/23/15.
+//  Copyright (c) 2015 Kelly Egan. All rights reserved.
+//
+
+import Foundation
+
+class ParseClient: NSObject {
+    
+    //Singleton
+    static let sharedInstance = ParseClient()
+    
+    
+
+}
---
+++ OnTheMap/ParseConstants.swift
@@ -0,0 +1,9 @@
+//
+//  ParseConstants.swift
+//  OnTheMap
+//
+//  Created by Kelly Egan on 8/23/15.
+//  Copyright (c) 2015 Kelly Egan. All rights reserved.
+//
+
+import Foundation
