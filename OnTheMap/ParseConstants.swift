e288ab8b87247b0de9ab654b4e30ea26481919b8
Kelly Egan <kelly.egan@gmail.com>
2015-08-23 12:22:47 -0400

    Add ParseConstants.swift

--- OnTheMap/ParseConstants.swift
+++ OnTheMap/ParseConstants.swift
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+
+
+extension ParseClient {
+    struct Constants {
+        static let BaseURL: String = "https://api.parse.com/1/classes/StudentLocation"
+        static let AppID: String = "QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr"
+        static let APIKey: String = "QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY"
+    }
+    
+    struct Messages {
+        static let networkError = "Error connecting to Parse."
+    }
+}
